20aa90696121f12b279c216ad6ebb484ac64c114
MichaelaZammit <michaela.zammit.f33220@mcast.edu.mt>
2025-05-09 01:40:31 +0200

    Added Supplies in the navbar

--- includes/header.php
+++ includes/header.php
@@ -38,6 +38,36 @@ if (isset($pageStyles)) {
     <a href="index.php">Home</a>
     <a href="create.php">Create</a>
     <a href="profile.php">Profile</a>
+    
+      <div class="dropdown-wrapper">
+    <span class="dropdown-title">Supplies</span>
+    <div class="dropdown-list">
+      <a href="supplies/accessories.php">Accessories</a>
+      <a href="supplies/contact_lenses.php">Contact Lenses</a>
+      <a href="supplies/charity_shops.php">Charity Shops</a>
+      <a href="supplies/corsets.php">Corsets</a>
+      <a href="supplies/dancewear_leotards.php">Dancewear & Leotards</a>
+      <a href="supplies/electronics_lighting.php">Electronics & Lighting</a>
+      <a href="supplies/fabric_notions.php">Fabric & Notions</a>
+      <a href="supplies/fiberglass_fabrication.php">Fiberglass & Fabrication</a>
+      <a href="supplies/foam_pvc.php">Foam & PVC</a>
+      <a href="supplies/jewelry_making.php">Jewelry-making</a>
+      <a href="supplies/leatherworking.php">Leatherworking</a>
+      <a href="supplies/miscellaneous.php">Miscellaneous</a>
+      <a href="supplies/paints_dyes.php">Paints & Dyes</a>
+      <a href="supplies/patterns.php">Patterns</a>
+      <a href="supplies/makeup.php">Makeup</a>
+      <a href="supplies/power_tools_equipment.php">Power Tools & Equipment</a>
+      <a href="supplies/prosthetics.php">Prosthetics</a>
+      <a href="supplies/sculpting_casting.php">Sculpting & Casting</a>
+      <a href="supplies/shoes.php">Shoes</a>
+      <a href="supplies/thermoplastics_fosshape.php">Thermoplastics & Fosshape</a>
+      <a href="supplies/tights_socks.php">Tights & Socks</a>
+      <a href="supplies/trims.php">Trims</a>
+      <a href="supplies/wigs_hair.php">Wigs & Hair</a>
+    </div>
+  </div>
+
   </div>
   <div class="nav-right">
     <form class="search-form" action="search.php" method="GET">
